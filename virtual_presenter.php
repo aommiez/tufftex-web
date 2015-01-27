5e697a8a1f574910616aa04688fbe62847140c79
NUIZ <papangping@gmail.com>
2015-01-27 15:52:48 +0700

    web v1.1

---
+++ virtual_presenter.php
@@ -0,0 +1,123 @@
+<html lang="en" class="swipebox-no-touch" style="
+    height: 100%;
+"><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Tufftex Group</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font -->
+    <link href="fonts02/font02.css" rel="stylesheet">
+    <link href="css/tufftex-component.css" rel="stylesheet">
+    <link href="css/tufftex.css" rel="stylesheet">
+    <link href="css/animate.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <!--<script src="js/jquery.nicescroll.min.js"></script>-->
+
+    <!--<link rel="stylesheet" href="swipebox-master/src/css/swipebox.min.css">-->
+    <!--<script src="swipebox-master/src/js/jquery.swipebox.min.js"></script>-->
+
+    <style>
+        @font-face {
+            font-family: thaisanslite;
+            src: url(fonts/thaisanslite_r1.ttf);
+        }
+
+        body {
+            font-family: Cocogoose, thaisanslite;
+        }
+
+        /** nav **/
+        .navbarBg {
+            background: black;
+        }
+
+        .navbar.fontBlack .navbar-nav > li > a {
+            color: white;
+        }
+        /** end nav **/
+
+        .item-text {
+            -webkit-animation-delay: 500ms;
+            -moz-animation-delay: 500ms;
+            -o-animation-delay: 500ms;
+            animation-delay: 500ms;
+        }
+
+        .item-icon {
+            -webkit-animation-duration: 2s;
+            box-shadow: -3px 0 15px rgba(0, 0, 0, 0.44);
+            border-radius: 24px;
+        }
+
+        .item-icon2 {
+            -webkit-animation-duration: 2s;
+            -webkit-animation-delay: 500ms;
+            box-shadow: -3px 0 15px rgba(0, 0, 0, 0.44);
+            border-radius: 15px;
+            margin-left: 20px;
+        }
+
+        .item-text {
+            -webkit-animation-duration: 2s;
+        }
+    </style>
+</head>
+
+<body role="document" style="height: 100%; background: black;">
+
+<!-- Fixed navbar -->
+<nav class="navbar navbar-fixed-top fontBlack">
+    <div class="navbarBg">
+
+    </div>
+    <div class="container">
+        <div class="navbar-header">
+            <a href="#">
+            </a><a href="#main">
+                <img src="images/head/logo.png" class="logoImg">
+            </a>
+
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="liMain active"><a href="index.html#main">HOME</a></li>
+                <li class="liAbout"><a href="index.html#aboutus">ABOUT US</a></li>
+                <li class="liPortfolio"><a href="index.html#portfilios">PRODUCTS</a></li>
+                <li class="liService"><a href="index.html#services">SERVICES</a></li>
+                <li class="liService"><a href="index.html#ourclients">OUR CLIENTS</a></li>
+                <li class="liContact"><a href="index.html#contact">CONTACT</a></li>
+            </ul>
+            <ul class="nav navbar-nav" style="float: right;">
+                <li><a href="#" style="font-size: 44px;font-weight: normal;padding: 12px 0px 12px 0px;color:#ffffff;">|</a></li>
+                <li><a href="#"><img src="images/head/iconFacebook.png"></a></li>
+                <li><a href="#"><img src="images/head/iconyoutube.png"></a></li>
+                <li><a href="#"><img src="images/head/icongmail.png"></a></li>
+            </ul>
+        </div>
+        <!--/.nav-collapse -->
+
+    </div>
+</nav>
+<div style="padding-top: 105px;">
+    <!--    <strong style="font-size: 33px; vertical-align: middle; padding: 0 20px; font-weight: bold;">Contruction</strong>-->
+</div>
+<div class="container">
+    <video src="images/vdo/full/virtual-presenterLOW.mp4" autoplay controls width="100%"></video>
+</div>
+</body>
+</html>
